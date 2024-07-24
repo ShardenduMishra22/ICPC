3194e913e2c9419cac7ad00d7aceeb325f167631
Shardendu Mishra <shardendumishra01@gmail.com>
2024-07-24 15:48:47 +0530

    ICPC Preparation

--- dfs.cpp
+++ dfs.cpp
@@ -68,6 +68,7 @@ void dfs(int vertex){
     for(auto child : g[vertex]){
         // Take action on child 
         // before entering the child node 
+        cout<<"Parent "<<vertex<<" Child "<<child;nl;
         if(vis[child]) continue;
 
         dfs(child);
@@ -78,7 +79,22 @@ void dfs(int vertex){
     // Take action on vertex before entering vertex
 }
 
-int main(){
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    for(int i = 0; i < m; i++) {
+        int v1, v2;
+        cin >> v1 >> v2;
+        g[v1].push_back(v2);
+        g[v2].push_back(v1);
+    }
+
+    for(int i = 1; i <= n; i++) {
+        if(!vis[i]) {
+            dfs(i);
+        }
+    }
 
     return 0;
 }
