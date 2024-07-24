03a0f9bf8feb6dc0d6a2a5afd6985ddb1bf1e0e3
Shardendu Mishra <shardendumishra01@gmail.com>
2024-07-25 04:23:09 +0530

    ICPC Preparation

--- SubTree.cpp
+++ SubTree.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-static auto _ = [] () {
+static auto _ = []() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
@@ -53,12 +53,63 @@ int size_arr = 0;
 int size_ll = 0;
 int top = -1;
 
+c_int N = 1e5 + 10;
+vint g[N];
+vbl Prime(N + 1, true);
 
-
-int main(){
-    int Nodes;cin>>Nodes;
-    frw(i,Nodes){
-
+void isPrime() {
+    Prime[0] = Prime[1] = false;
+    for (int i = 2; i * i <= N; i++) {
+        if (Prime[i]) {
+            for (int j = i * i; j <= N; j += i) {
+                Prime[j] = false;
+            }
+        }
     }
+}
+
+void dfs(int vrtx, int prnt, vint& SubTree, vint& OddCount, vint& EvenCount, vint& PrimeCount) {
+    SubTree[vrtx] = vrtx;
+    if (vrtx & 1) OddCount[vrtx] = 1;
+    else EvenCount[vrtx] = 1;
+    if (Prime[vrtx]) PrimeCount[vrtx] = 1;
+
+    for (auto child : g[vrtx]) {
+        if (child == prnt) continue;
+        dfs(child, vrtx, SubTree, OddCount, EvenCount, PrimeCount);
+        SubTree[vrtx] += SubTree[child];
+        OddCount[vrtx] += OddCount[child];
+        EvenCount[vrtx] += EvenCount[child];
+        PrimeCount[vrtx] += PrimeCount[child];
+    }
+}
+
+int main() {
+    int Nodes; cin >> Nodes;
+    frw(0, Nodes - 1) {
+        int u, v;
+        cin >> u >> v;
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+
+    isPrime();
+
+    vint SubtreeSum(Nodes + 1, 0);
+    vint OddCount(Nodes + 1, 0);
+    vint EvenCount(Nodes + 1, 0);
+    vint PrimeCount(Nodes + 1, 0);
+
+    dfs(1, 0, SubtreeSum, OddCount, EvenCount, PrimeCount);
+
+    frw(1, Nodes + 1) {
+        cout << "Node " << i << ":\n";
+        cout << "Subtree sum: " << SubtreeSum[i] << "\n";
+        cout << "Odd count: " << OddCount[i] << "\n";
+        cout << "Even count: " << EvenCount[i] << "\n";
+        cout << "Prime count: " << PrimeCount[i] << "\n";
+        nl
+    }
+
     return 0;
 }
